66a8cd4e81b593e0b210fc8fa0cadd9f178a9d0a
Danilo Blum <dnl.blm@gmail.com>
2024-08-13 21:19:24 -0300

    home, ajustes nas bordas customizadas

--- assets/js/home.js
+++ assets/js/home.js
@@ -14,3 +14,16 @@ window.addEventListener('scroll', function (event) {
 		layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
 	}
 });
+
+window.addEventListener('scroll', function (event) {
+	var top = this.pageYOffset;
+
+	var layers = document.getElementsByClassName('parallax-scale');
+	var layer, speed, yPos;
+	for (var i = 0; i < layers.length; i++) {
+		layer = layers[i];
+		speed = layer.getAttribute('data-speed');
+		var yPos = 1 + (top * speed) / 100;
+		layer.setAttribute('style', 'transform: scale(' + yPos + ')');
+	}
+});
